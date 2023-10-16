2bf4f8f50122f2046a7fd380359c27c0a47e6212
blackangoo <blackangoo.gc@gmail.com>
2023-10-16 20:33:58 +0200

    added comments to salt.py

--- security23_as1/src/salt.py
+++ security23_as1/src/salt.py
@@ -1,5 +1,8 @@
 from Crypto.Random import get_random_bytes
+
 '''
+# Run this if you need to reset the nonce and the salt 
+
 salt = get_random_bytes(16)
 nonce = get_random_bytes(12)
 
@@ -9,12 +12,25 @@ with open('utils/salt.txt', 'wb') as file:
 with open('utils/nonce.txt', 'wb') as file:
     file.write(nonce)
 '''
+
 def get_salt():
+    """
+    Reads and returns the salt value stored in a file.
+    
+    Returns:
+        bytes: The salt value as bytes.
+    """
     with open('utils/salt.txt', 'rb') as file:
         salt = file.read()
     return salt
 
 def get_nonce():
+    """
+    Reads and returns the nonce value stored in a file.
+    
+    Returns:
+        bytes: The nonce value as bytes.
+    """
     with open('utils/nonce.txt', 'rb') as file:
         nonce = file.read()
     return nonce
