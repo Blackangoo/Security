7a6b9ee6d366f28aaf37204dd56b009091608ee9
blackangoo <blackangoo.gc@gmail.com>
2023-10-16 20:38:29 +0200

    added comments to rsa.py

--- security23_as1/src/rsa.py
+++ security23_as1/src/rsa.py
@@ -1,13 +1,21 @@
 from Crypto.Util.number import getPrime
 
 def generate_keys():
+    """
+    Generates RSA key pairs and saves them to 'private.txt' and 'public.txt' files (Using textbook RSA).
+    
+    """
     p = getPrime(1500)
     q = getPrime(1500)
+
     n = p * q
     phiN = (p - 1) * (q - 1)
     e = 65537
     d = pow(e, -1, phiN)
-    private = open("private.txt", "w")
-    public = open("public.txt", "w")
-    private.write(str((d, n)))
-    public.write(str((e,n)))
+
+    with open("keys/private.txt", "w") as private_file, open("keys/public.txt", "w") as public_file:
+        # Write the private key (d, n) to the private key file
+        private_file.write(str((d, n)))
+        
+        # Write the public key (e, n) to the public key file
+        public_file.write(str((e, n)))
