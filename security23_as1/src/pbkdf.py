74fd94d55ef856fffb4d53debc1630043157b8b0
blackangoo <blackangoo.gc@gmail.com>
2023-10-16 20:28:02 +0200

    added comments to pbkdf.py

--- security23_as1/src/pbkdf.py
+++ security23_as1/src/pbkdf.py
@@ -2,6 +2,16 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA512
 
 def derive_key_from_password(password, salt):
-    keys = PBKDF2(password, salt, 64, count=1000000, hmac_hash_module=SHA512)
+    """
+    Derives a cryptographic key from a password using PBKDF2 (Password-Based Key Derivation Function).
+
+    Args:
+        password (str or bytes): The secret password from which to generate the key.
+        salt (str or bytes): A random or unique string or bytes to enhance protection against dictionary attacks.
+
+    Returns:
+        bytes: A derived key with a length of 16 bytes
+    """
+    keys = PBKDF2(password, salt, 32, count=1000000, hmac_hash_module=SHA512)
     key = keys[:16]  # La clé générée de 32 octets (256 bits)
     return key
