97d41daecd78832ee7cf09f3212b4f54c42e6fcc
blackangoo <blackangoo.gc@gmail.com>
2023-10-17 16:54:59 +0200

    added clean_tests_directory.py

---
+++ security23_as1/tests/clean_tests_directory.py
@@ -0,0 +1,19 @@
+import os
+
+def clean_tests_directory():
+    directory = '.'
+
+    # List of files to preserve
+    files_to_preserve = ['Examples.txt', 'SecretImage.png', 'SecretProgram.exe', 'SecretText.txt', 'clean_tests_directory.py']
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path) and filename not in files_to_preserve:
+            try:
+                os.remove(file_path)
+                print(f"Removed file: {file_path}")
+            except Exception as e:
+                print(f"Error deleting file: {file_path} - {e}")
+
+if __name__ == '__main__':
+    clean_tests_directory()
